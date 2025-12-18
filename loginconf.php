65b958ac5c4884d817ad2d70aa76d681d6ad9b72
Alfredo212121 <alfredojose167@gmail.com>
2025-12-18 20:29:55 +0100

    Update author meta tag in loginconf.php

--- loginconf.php
+++ loginconf.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <meta name="author" content="Manuel Ortiz Moreno">
+     <meta name="author" content="Alfredo Rivadulla Fuenmayor">
     <title>EXSQL</title>
 </head>
 <body>
